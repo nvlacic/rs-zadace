72622d6a993062c193e0b69deb7221663a079e5b
nvlacic <nvlacic@unipu.hr>
2024-11-26 14:39:46 +0100

    Create Vjezba6.py

---
+++ Vjezba6.py
@@ -0,0 +1,11 @@
+for i in range(1, 2):
+    print(i)
+# petlja ispisuje samo 1 zbog zadanog raspona
+
+for i in range(1, 10, 2):
+    print(i)
+# petlja ispisuje brojeve 1, 1, 3, 5, 7, 9
+
+for i in range(10, 1, -1):
+    print(i)
+# petlja ispisuje sve brojeve od 10 do 2 (10, 9, 8, 7, 6, 5, 4, 3, 2)
